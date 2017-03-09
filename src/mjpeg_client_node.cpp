8cb11b93235f428b04f769d122e452027d2f4b48
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2017-03-09 15:12:07 +0900

    format source files

--- src/mjpeg_client_node.cpp
+++ src/mjpeg_client_node.cpp
@@ -4,15 +4,15 @@
 #include <ros/this_node.h>
 
 int main(int argc, char *argv[]) {
-    ros::init(argc, argv, "mjepg_client_node", ros::init_options::AnonymousName);
+  ros::init(argc, argv, "mjepg_client_node", ros::init_options::AnonymousName);
 
-    ros::V_string args;
-    ros::removeROSArgs(argc, argv, args);
+  ros::V_string args;
+  ros::removeROSArgs(argc, argv, args);
 
-    mjpeg_client::MjpegClient node;
-    node.init(ros::this_node::getName(), ros::names::getRemappings(), args);
+  mjpeg_client::MjpegClient node;
+  node.init(ros::this_node::getName(), ros::names::getRemappings(), args);
 
-    ros::spin();
-    
-    return 0;
+  ros::spin();
+
+  return 0;
 }
